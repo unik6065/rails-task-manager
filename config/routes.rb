4f7106b94689ef201f34aa15e474e70b4d3de728
romain isoz <isozromain@gmail.com>
2023-02-21 11:23:30 +0100

    ajout de l'option add task au projet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # read all
   get 'tasks', to: 'tasks#index'
+  # create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
