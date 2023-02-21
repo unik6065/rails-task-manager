670683b7543d3cf0ed85210d0566dc108c819e9f
romain isoz <isozromain@gmail.com>
2023-02-21 11:57:59 +0100

    ajout de la fonctionnalité edit a task

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
   # read one
   get 'tasks/:id', to: 'tasks#show', as: :task
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 end
