a3f421dc98dcc9faa19aa4fbe99f17ce86fc4ae3
romain isoz <isozromain@gmail.com>
2023-02-21 12:14:11 +0100

    ajout de la fonction delete au projet

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   # update
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
